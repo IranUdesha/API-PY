efdad3b9b2d78a7f20aa1e7c057ec14fc2ae0c83
Iran Udesha <iran.u@hsenidbiz.com>
2023-12-27 19:54:14 +0530

    Added Middleware to save access log

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from fastapi import FastAPI, HTTPException, Security, status
+#install fastapi -  pip install fastapi[all]
+#install uvicorn - pip install uvicorn[standard]
+#pip install uvicorn[standard] certifi
+
+
+from fastapi import FastAPI, HTTPException, Request, Security, status
 from fastapi.security import APIKeyHeader
 import uvicorn
 
 import json
 from fastapi.responses import JSONResponse
+import logging
 
 server_list = "server_list.json"
 
@@ -11,16 +17,68 @@ api_keys = [
     "API-Key"
 ]
 
-def read_json_file(file_path):# read data from JSON file
-        with open(file_path, 'r') as file:
-            data = json.load(file)
-        return data
+
+# Set up logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler("logs/access.log"),
+        logging.StreamHandler()
+    ]
+)
 
 app = FastAPI()
 
-api_key_header = APIKeyHeader(name="Authentication")  #API key header
 
-def get_api_key(api_key_header: str = Security(api_key_header)) -> str: #Get API Key from the header
+@app.middleware("https")
+async def log_requests(request: Request, call_next):
+    """
+    Log the request to the access log.
+
+    Args:
+        request (Request): The request.
+        call_next (function): The next function to call.
+
+    Returns:
+        Response: The response.
+    """
+    logging.info(f"{request.client.host} - {request.method} - {request.url.path}")
+    return await call_next(request) # pragma: no cover
+
+
+
+def read_json_file(file_path):
+    """
+    Read data from a JSON file.
+
+    Args:
+        file_path (str): The path to the JSON file.
+
+    Returns:
+        dict: The data read from the JSON file.
+    """
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
+
+
+
+api_key_header = APIKeyHeader(name="Authentication")
+
+def get_api_key(api_key_header: str = Security(api_key_header)) -> str:
+    """
+    Get the API Key from the header.
+
+    Args:
+        api_key_header (str): The API Key header.
+
+    Returns:
+        str: The API Key.
+
+    Raises:
+        HTTPException: If the API Key is invalid or missing.
+    """
     if api_key_header in api_keys:
         return api_key_header
     raise HTTPException(
@@ -30,12 +88,20 @@ def get_api_key(api_key_header: str = Security(api_key_header)) -> str: #Get API
 
 @app.get("/api/data")
 def data(api_key: str = Security(get_api_key)):
-    # Process the request for authenticated users
-        server_list_data = read_json_file(server_list)
-        return JSONResponse(content=server_list_data)
-    
-    # return read_json_file(server_list)
+    """
+    Process the request for authenticated users and return server list data.
+
+    Args:
+        api_key (str): The API Key.
+
+    Returns:
+        JSONResponse: The server list data as a JSON response.
+    """
+    server_list_data = read_json_file(server_list)
+    return JSONResponse(content=server_list_data)
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='localhost', port=80, headers=[('Access-Control-Allow-Origin', '*'), ('server', ' ')],)
+    print("Server is running")
+    uvicorn.run(app, host='localhost', port=80, headers=[('Access-Control-Allow-Origin', '*'), ('server', ' ')])
 
+    #print(reply)
